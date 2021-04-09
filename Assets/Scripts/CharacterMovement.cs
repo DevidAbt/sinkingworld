f7dcaa41300d5e311af1b816f48e0cc129d7fd17
Devid4 <abt.devid@gmail.com>
2021-04-10 00:34:06 +0200

    Basic animations

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,11 +7,23 @@ public class CharacterMovement : MonoBehaviour
     public static bool facingRight = true;
 
     private Rigidbody2D rb;
+    private Animator animator;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    private bool doubleJumped = false;
+    public float timeSpentJumping = 0f;
+    public float doubleJumpStartTime = 1f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,10 +38,30 @@ public class CharacterMovement : MonoBehaviour
             flip();
         }
 
-        if (Input.GetButtonDown("Jump"))
+        bool grounded = Physics2D.OverlapCircle(groundCheck.position,
+                                                groundRadius,
+                                                whatIsGround);
+
+        if (Input.GetButtonDown("Jump") && (grounded || !doubleJumped))
         {
+            timeSpentJumping += Time.deltaTime;
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            Debug.Log(timeSpentJumping);
+            if (timeSpentJumping > doubleJumpStartTime && !grounded)
+            {
+                doubleJumped = true;
+                animator.SetTrigger("DoubleJump");
+            }
         }
+
+        if (grounded)
+        {
+            doubleJumped = false;
+            timeSpentJumping = 0;
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(this.rb.velocity.x));
+        animator.SetBool("Grounded", grounded);
     }
 
     private void flip()
