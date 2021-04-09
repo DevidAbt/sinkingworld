1f79e98be0d3e3d173649393b67c64e29064c4a2
Devid4 <abt.devid@gmail.com>
2021-04-09 15:44:54 +0200

    Deactivate platform

--- Assets/Scripts/PlatformPool.cs
+++ Assets/Scripts/PlatformPool.cs
@@ -7,11 +7,12 @@ public class PlatformPool : MonoBehaviour
     public BoxCollider2D itemProto = null;
 
     private Vector3 startPosition;
+    private Vector3 endPosition;
     public Vector3 bias = new Vector3(0, 0, 0);
 
     public int itemPoolSize = 1;
-    private Stack<BoxCollider2D> activeItemPool;
-    private Stack<BoxCollider2D> passiveItemPool;
+    private Queue<BoxCollider2D> activeItemPool;
+    private Queue<BoxCollider2D> passiveItemPool;
 
     private float nextActionTime = 0.0f;
     public float period = 1f;
@@ -29,20 +30,23 @@ public class PlatformPool : MonoBehaviour
         startPosition = platformStart.transform.position;
         startPosition += bias;
 
+        GameObject platformEnd = GameObject.FindGameObjectWithTag("PlatformEnd");
+        endPosition = platformEnd.transform.position;
+
         Quaternion startRotation = new Quaternion();
 
         platformWidth = itemProto.GetComponent<SpriteRenderer>().bounds.size.x * 3 + 1;
 
         random = new System.Random();
 
-        activeItemPool = new Stack<BoxCollider2D>();
-        passiveItemPool = new Stack<BoxCollider2D>();
+        activeItemPool = new Queue<BoxCollider2D>();
+        passiveItemPool = new Queue<BoxCollider2D>();
         for (int i = 0; i < itemPoolSize; i++)
         {
             // Vector3 position = new Vector3(startTransform.position.x, startTransform.position.y, startTransform.position.z);
             BoxCollider2D boxCollider = Instantiate(itemProto, startPosition, startRotation);
             boxCollider.gameObject.SetActive(false);
-            passiveItemPool.Push(boxCollider);
+            passiveItemPool.Enqueue(boxCollider);
         }
     }
 
@@ -57,26 +61,38 @@ public class PlatformPool : MonoBehaviour
             Debug.Log($"Tick: {tick}");
             if (tick % 3 == 1)
             {
-                BoxCollider2D newCollider = passiveItemPool.Pop();
+                BoxCollider2D newCollider = passiveItemPool.Dequeue();
                 if (newCollider)
                 {
                     newCollider.gameObject.SetActive(true);
                     int randomPos = this.random.Next(0, 2);
                     newCollider.transform.position = new Vector3(startPosition.x + platformWidth * randomPos, startPosition.y, startPosition.z);
-                    activeItemPool.Push(newCollider);
+                    activeItemPool.Enqueue(newCollider);
                     Debug.Log($"Added platform ({tick})");
                 }
             }
 
+            bool removeOne = false;
             foreach (BoxCollider2D item in activeItemPool)
             {
                 item.transform.position += new Vector3(0, -ascendingDist, 0);
-            }
 
+                if (item.transform.position.y < endPosition.y)
+                {
+                    removeOne = true;
+                }
+            }
+            if (removeOne)
+            {
+                BoxCollider2D removedCollider = activeItemPool.Dequeue();
+                removedCollider.gameObject.SetActive(false);
+                passiveItemPool.Enqueue(removedCollider);
+            }
         }
     }
 
-    public Stack<BoxCollider2D> getActiveItemPool() {
+    public Queue<BoxCollider2D> getActiveItemPool()
+    {
         return activeItemPool;
     }
 }
